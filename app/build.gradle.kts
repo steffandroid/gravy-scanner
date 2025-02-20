a0d011c4174a621bf4498c1c4db751e0286352be
Steffan Davies <steff@steffandroid.co.uk>
2025-02-20 19:31:58 +0100

    Increment version to 0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "uk.co.steffandroid.gravyscanner"
         minSdk = 21
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
